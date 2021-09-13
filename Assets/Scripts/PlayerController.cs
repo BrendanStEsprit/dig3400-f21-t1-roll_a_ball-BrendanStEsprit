a9681741ff79a8a648026096d79778eab5d41127
Brendan <weegeegotme@gmail.com>
2021-09-13 11:59:04 -0400

    September 13, 2021 11:59 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,18 +11,28 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     public Text countText;
+    public Text livesText;
     public Text winText;
+    public Text loseText;
     private int count;
+    private int lives;
     private float movementX;
     private float movementY;
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
         count = 0;
-        countText.text = "Count: " + count.ToString();
-        SetCountText ();
+        lives = 3;
         winText.text = "";
+        loseText.text = "";
+        countText.text = "Count: " + count.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+        SetCountText();
+        SetLivesText();
+        
+        
     }
 
     void OnMove(InputValue movementValue)
@@ -45,16 +55,49 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive (false);
             count = count + 1;
-            SetCountText ();
+            SetCountText();
         }
+       else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+
+        }
+                
+       
        
     }
     void SetCountText ()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 10)
+        if (count >= 16)
         {
             winText.text = "You're Winner!, Made By Brendan St.Esprit";
         }
+        if (count >= 10)
+        {
+            transform.position = new Vector3(-8.65f, 15.3f, 96.7f); 
+        }
+
     }
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+
+            loseText.text = "Sorry, You lost everyting";
+            SetLivesText();
+            
+        }
+    }
+    void DestroyScriptInstance()
+    {
+        if (lives == 0)
+        {
+            Destroy(this);
+        }
+    }
+
 }
